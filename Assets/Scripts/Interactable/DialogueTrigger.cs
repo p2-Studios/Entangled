ee21d699e502d873dd76c11ecc5887368aeb369a
Dakotaa <kotalalach@gmail.com>
2022-02-26 10:57:07 -0600

    Applied assets for Terminal

--- Assets/Scripts/Interactable/DialogueTrigger.cs
+++ Assets/Scripts/Interactable/DialogueTrigger.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class DialogueTrigger : Interactable {
     public Dialogue dialogue;
     private DialogueManager dialogueManager;
+    public Animator animator;
 
     public string triggerSound = "";    // the sound to play when the dialogue is triggered
     
     public void Trigger() {
         dialogueManager.StartDialogue(dialogue);
+        animator.SetBool("messageRead", true);
         if (triggerSound.Length != 0) FindObjectOfType<AudioManager>().Play(triggerSound); // play trigger sound
     }
 
