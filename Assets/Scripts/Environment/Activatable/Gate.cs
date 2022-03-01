34a4d60e1f8d284adc89ff3d0d39e5d975807805
jiw537 <jiw537@usask.ca>
2022-03-01 01:26:18 -0600

    Added invert activation option for gate script, ignorable colliders for pressure pad script, and a testing-level

--- Assets/Scripts/Environment/Activatable/Gate.cs
+++ Assets/Scripts/Environment/Activatable/Gate.cs
@@ -3,67 +3,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Activation_System
-{
-    public class Gate : Activatable
-    {
-        public Activator[] activators;			// -- array of activators, REQUIRED to set the activators manually! --
-		
+namespace Activation_System {
+	public class Gate : Activatable {
+		public Activator[] activators;          // -- array of activators, REQUIRED to set the activators manually! --
+		public bool invert_Activation = false;  // Inverts the activation of the gate 
+
 		// Animation variables
 		private Animator gateAnimator;
-		private float timer = 0.1f;				// timer when gate moves
-		
+		private float timer = 0.1f;             // timer when gate moves
+
 		// Gate collider
-		BoxCollider2D gate;						// Blocks rigidbodies and other colliders from passing
-		
+		BoxCollider2D gate;                     // Blocks rigidbodies and other colliders from passing
+
 		// Boolean state
-		private bool active = false;			// check when the gate is activated or not
-		private int state = 1;					// the current state of the gate, used for animators
-		
+		private bool active = false;            // check when the gate is activated or not
+		private int state = 1;                  // the current state of the gate, used for animators
+
 		void Start() {
-			
+
 			gateAnimator = GetComponent<Animator>();
 			// animationSpeed = gateAnimator.speed;
-			
+
 			gate = GetComponent<BoxCollider2D>();
-			
+
 			foreach (Activator a in activators) {
 				AddActivator(a);
 			}
+
+			if (invert_Activation) {
+				state = 11;
+				gateAnimator.Play("11");
+			}
+
 		}
-		
+
 		bool isOpened() {
 			return active;
 		}
-		
+
 		bool isClosed() {
 			return !active;
 		}
-		
+
 		void setOpen() {
 			active = true;
 			gate.enabled = false;
 		}
-		
+
 		void setClose() {
 			active = false;
 			gate.enabled = true;
 		}
 
-		void FixedUpdate() {							// NOTE: OnTrigger event is timed on FixedUpdate
-			if (!active && IsActivated()) {
+		void FixedUpdate() {                            // NOTE: OnTrigger event is timed on FixedUpdate
+			if ((!active && IsActivated() && !invert_Activation) || (invert_Activation && !active && !IsActivated())) {
 				setOpen();
 			}
-			else if (active && !IsActivated()) {
+			else if ((active && !IsActivated() && !invert_Activation) || (invert_Activation && active && IsActivated())) {
 				setClose();
 			}
-			
+
 			// Play's animation
 			if (isOpened() && state != 11) {
 				timer -= Time.deltaTime;
 				if (timer <= 0) {
 					state += 1;
-					gateAnimator.SetInteger("state",state);
+					gateAnimator.SetInteger("state", state);
 					timer = 0.1f;
 				}
 			}
@@ -71,12 +76,12 @@ namespace Activation_System
 				timer -= Time.deltaTime;
 				if (timer <= 0) {
 					state -= 1;
-					gateAnimator.SetInteger("state",state);
+					gateAnimator.SetInteger("state", state);
 					timer = 0.1f;
 				}
 			}
-			
+
 		}
 
-    }
+	}
 }
--- Assets/Scripts/Environment/Activator/PressurePad.cs
+++ Assets/Scripts/Environment/Activator/PressurePad.cs
@@ -3,133 +3,144 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Activation_System
-{
-    public class PressurePad : Activator
-    {
-        public float requiredMass = 1.0f;								// required mass to trigger the pressure pad
-		
-		Dictionary<Collider2D, float> obj_mass;							// store object and gather masses
-		Dictionary<Collider2D, ArrayList> obj_stacked;					// store the object being stacked
-		
-		public Activatable[] activatables;								// -- array of activatables, REQUIRED to set the activatables manually! --
-		
+namespace Activation_System {
+	public class PressurePad : Activator {
+		public float requiredMass = 1.0f;                               // required mass to trigger the pressure pad
+
+		Dictionary<Collider2D, float> obj_mass;                         // store object and gather masses
+		Dictionary<Collider2D, ArrayList> obj_stacked;                  // store the object being stacked
+
+		public Activatable[] activatables;                              // -- array of activatables, REQUIRED to set the activatables manually! --
+
 		private BoxCollider2D padCollider;
-		
-		private Animator pressurePadAnimator;							// For animations
-		private float animationSpeed = 0.0f;							// This has no use currently
-		
+
+		private Animator pressurePadAnimator;                           // For animations
+		private float animationSpeed = 0.0f;                            // This has no use currently
+
+		public Collider2D[] ignore;
 		void Start() {
-			padCollider = GetComponent<BoxCollider2D>();				// fetch collider
-			
-			obj_mass = new Dictionary<Collider2D,float>();
-			
-			obj_stacked = new Dictionary<Collider2D,ArrayList>();
-			
+			padCollider = GetComponent<BoxCollider2D>();                // fetch collider
+
+			obj_mass = new Dictionary<Collider2D, float>();
+
+			obj_stacked = new Dictionary<Collider2D, ArrayList>();
+
 			pressurePadAnimator = GetComponent<Animator>();
-			
+
 			foreach (Activatable a in activatables) {
 				AddActivatable(a);
 			}
-			
+
 		}
-		
+
 		void OnTriggerEnter2D(Collider2D col) {
 			if (col.gameObject.CompareTag("Terrain")) return;
-			obj_mass.Add(col,col.gameObject.GetComponent<Rigidbody2D>().mass);				// The sum of all mass on the pressure pad
-			
-			obj_stacked.Add(col,null);
-			
+			foreach (Collider2D c in ignore) {
+				if (c == col)
+					return;
+			}
+			obj_mass.Add(col, col.gameObject.GetComponent<Rigidbody2D>().mass);             // The sum of all mass on the pressure pad
+
+			obj_stacked.Add(col, null);
+
 			float sumOfMass = 0.0f;
-			
-			foreach (KeyValuePair<Collider2D,float> kvp in obj_mass) {
+
+			foreach (KeyValuePair<Collider2D, float> kvp in obj_mass) {
 				sumOfMass += kvp.Value;
 			}
-			
+
 			if (!IsActivated()) {
-				if (requiredMass <= sumOfMass) {											// When the required mass target is reached
-					 Activate(animationSpeed);
-					 pressurePadAnimator.SetBool("active",true);
+				if (requiredMass <= sumOfMass) {                                            // When the required mass target is reached
+					Activate(animationSpeed);
+					pressurePadAnimator.SetBool("active", true);
 				}
 			}
 		}
-		
-		
+
+
 		void OnTriggerStay2D(Collider2D col) {
 			if (col.gameObject.CompareTag("Terrain")) return;
+			foreach (Collider2D c in ignore) {
+				if (c == col)
+					return;
+			}
 			float sumOfMass = 0.0f;
-			
+
 			obj_stacked[col] = new ArrayList();
-			
-			get_objects(obj_stacked[col], col.gameObject);	
-			
+
+			get_objects(obj_stacked[col], col.gameObject);
+
 			foreach (GameObject go in obj_stacked[col]) {
 				Rigidbody2D rb = go.GetComponent<Rigidbody2D>();
 				if (rb != null)
 					sumOfMass += rb.mass;
 			}
-			
+
 			obj_mass[col] = sumOfMass;
-			
-			foreach (KeyValuePair<Collider2D,float> kvp in obj_mass) {
+
+			foreach (KeyValuePair<Collider2D, float> kvp in obj_mass) {
 				if (kvp.Key != col)
 					sumOfMass += kvp.Value;
 			}
-			
+
 			if (!IsActivated()) {
-				if (requiredMass <= sumOfMass) {											// When the required mass target is reached
-					 Activate(animationSpeed);
-					 pressurePadAnimator.SetBool("active",true);
+				if (requiredMass <= sumOfMass) {                                            // When the required mass target is reached
+					Activate(animationSpeed);
+					pressurePadAnimator.SetBool("active", true);
 				}
 			}
 			else {
-				if (requiredMass > sumOfMass) {												// When the required mass target is not reached
+				if (requiredMass > sumOfMass) {                                             // When the required mass target is not reached
 					Deactivate(animationSpeed);
-					pressurePadAnimator.SetBool("active",false);
+					pressurePadAnimator.SetBool("active", false);
 				}
 			}
 		}
-		
+
 		void OnTriggerExit2D(Collider2D col) {
 			if (col.gameObject.CompareTag("Terrain")) return;
-			obj_mass.Remove(col);															// Remove from dictionary since no longer on pad
+			foreach (Collider2D c in ignore) {
+				if (c == col)
+					return;
+			}
+			obj_mass.Remove(col);                                                           // Remove from dictionary since no longer on pad
 			obj_stacked.Remove(col);
-			
+
 			float sumOfMass = 0.0f;
-			
-			foreach (KeyValuePair<Collider2D,float> kvp in obj_mass) {
+
+			foreach (KeyValuePair<Collider2D, float> kvp in obj_mass) {
 				sumOfMass += kvp.Value;
 			}
-			
+
 			if (IsActivated()) {
 				if (requiredMass > sumOfMass) {
 					Deactivate(animationSpeed);
-					pressurePadAnimator.SetBool("active",false);
+					pressurePadAnimator.SetBool("active", false);
 				}
 			}
-		
+
 		}
-		
+
 		// Used to fetch list of stacking Game Objects
 		void get_objects(ArrayList obj_array, GameObject g) {
-			
-			foreach (KeyValuePair<Collider2D,ArrayList> kvp in obj_stacked) {
-				if (kvp.Value != null && kvp.Value.Contains(g)) {				// Excludes any object already seen
+
+			foreach (KeyValuePair<Collider2D, ArrayList> kvp in obj_stacked) {
+				if (kvp.Value != null && kvp.Value.Contains(g)) {               // Excludes any object already seen
 					return;
 				}
 			}
-			
-			obj_array.Add(g);													// if hasn't been added before then add it
-			
+
+			obj_array.Add(g);                                                   // if hasn't been added before then add it
+
 			Collider2D col = g.GetComponent<Collider2D>();
-			
+
 			if (col == null) {
 				return;
 			}
 			else {
-				ContactPoint2D[] contacts = new ContactPoint2D[25];				// The maximum contacts to search
+				ContactPoint2D[] contacts = new ContactPoint2D[25];             // The maximum contacts to search
 				col.GetContacts(contacts);
-				
+
 				foreach (ContactPoint2D cp in contacts) {
 					if (cp.collider != null) {
 						if (cp.normal.y < -0.3f)
@@ -138,6 +149,6 @@ namespace Activation_System
 				}
 			}
 		}
-		
-    }
+
+	}
 }
