f3ee556e4a90c5fa54c3fd1f39992854cc7c41a4
Matthew S <mds569@usask.ca>
2022-03-16 00:59:38 -0600

    CollectorUI script (not completed)

---
+++ Assets/Scripts/UI/CollectorUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectorUI : MonoBehaviour
+{
+
+    [SerializeField] private Text orbsText;
+    private Level levelData;
+    private string collected;
+    private string total;
+    private int colNum;
+    private int totNum;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        changeText();
+    }
+
+    private void Awake() {
+        levelData = FindObjectOfType<Level>();
+        if (levelData == null) {
+           orbsText.enabled = false;
+        }
+        else {
+            orbsText.enabled = true;
+        }
+        changeText();
+    }
+
+    void changeText() {
+        colNum = levelData.GetFoundOrbsCount();
+        totNum = levelData.GetTotalOrbCount();
+        collected = "Orbs Collected: " + colNum.ToString();
+        total = "  ||  Total Orbs: " + totNum.ToString();
+        orbsText.text = collected + total;
+    }
+}
