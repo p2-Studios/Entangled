71555c9f175988e374027bcfb827a35cf7883ebf
Dakotaa <kotalalach@gmail.com>
2022-03-22 22:09:14 -0600

    Added AutolinkingRespawner prefab

--- Assets/Scripts/Entanglement/RespawnerLinker.cs
+++ Assets/Scripts/Entanglement/RespawnerLinker.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RespawnerLinker : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class RespawnerLinker : MonoBehaviour {
+    void Awake() {
+        ArrayList objects = new ArrayList();
+        Transform respawner = transform;
+        foreach (Transform child in transform) {
+            if (child.gameObject.name.Equals("ObjectRespawner")) {
+                respawner = child;
+            } else {
+                Entanglable e = child.GetComponentInChildren<Entanglable>();
+                if (e != null) {
+                    objects.Add(e);
+                }
+            }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        foreach (Entanglable e in objects) {
+            e.respawnLocation = respawner;
+        }
     }
 }
